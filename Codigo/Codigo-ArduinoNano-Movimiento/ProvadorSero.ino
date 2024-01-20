80122caf62a5783f648f236a42ddbdf7dded7bfb
AryGimenez <argi.prog@gmail.com>
2024-01-20 19:16:52 -0300

    Voy a iniciar a agregar código para probar los diferentes componentes del proyecto - I'm going to start adding code to test the different components of the project

---
+++ Codigo/Codigo-ArduinoNano-Movimiento/ProvadorSero.ino
@@ -0,0 +1,23 @@
+#include <Servo.h>
+
+Servo miServo;
+int velocidad = 0;  // Variable para almacenar la "velocidad"
+
+void setup() {
+  Serial.begin(9600);  // Inicia la comunicación serial
+  miServo.attach(11);  // Adjunta el servo al pin D11
+  Serial.println("Ingrese la velocidad (-90 a 90) y presione enviar:");
+}
+
+void loop() {
+  if (Serial.available() > 0) {  // Chequea si hay datos disponibles para leer en la terminal serial
+    velocidad = Serial.parseInt();  // Lee la "velocidad" enviada
+
+    // Convierte la "velocidad" a un valor de microsegundos
+    int valorPWM = map(velocidad, -90, 90, 1000, 2000);
+    miServo.writeMicroseconds(valorPWM);
+
+    Serial.println("Ingrese la próxima velocidad:");
+    Serial.flush();  // Limpia el buffer serial para la próxima lectura
+  }
+}
